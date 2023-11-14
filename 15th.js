eadc2c1da20dac2175afdbb96d70d5640463699e
uwajeneza5 <148440535+uwajeneza5@users.noreply.github.com>
2023-11-14 15:40:53 +0200

    exercises

---
+++ 15th.js
@@ -0,0 +1,10 @@
+function reverseArray(array) {
+  return array.reverse();
+}
+
+// Example usage:
+var array = [1, 3, 4, 3, 7, 8, 0, 12, 19];
+console.log("Original array:", array);
+
+var reversedArray = reverseArray(array);
+console.log("Reversed array:", reversedArray);
