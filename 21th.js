b7f070b966abf7b64dc127c0351f6f7a54442b44
uwajeneza5 <148440535+uwajeneza5@users.noreply.github.com>
2023-11-14 12:48:13 +0200

    exercises

---
+++ 21th.js
@@ -0,0 +1,8 @@
+function squareRoot(number) {
+  return Math.sqrt(number);
+}
+
+// Example usage:
+const number = 25;
+const result = squareRoot(number);
+console.log(`The square root of ${number} is: ${result}`);
